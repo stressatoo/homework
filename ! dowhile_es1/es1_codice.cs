9a973c98154420f8fe5d8ad52ffa063ddc9b0684
Alessandro <60473020+stressatoo@users.noreply.github.com>
2022-04-21 12:26:32 +0200

    DO WHILE
    //

---
+++ ! dowhile_es1/es1_codice.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace dispari
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEsegui_Click(object sender, EventArgs e)
+        {
+            int nPari    = 0;
+            int nDispari = 0;
+
+            int input;
+
+            do
+            {
+                input = Convert.ToInt32(Interaction.InputBox("Inserisci numero"));
+
+                if (input % 2 == 0)
+                {
+                    nPari++;
+                }
+                else nDispari++;
+
+            } while (nDispari < 1);
+
+            txtNPari.Text = nPari.ToString();
+        }
+    }
+}
---
+++ ! dowhile_es2/es2_codice.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+/*
+ * Programma che legge una serie di numeri interi positivi
+ * arrestandosi quando la somma dei numeri immessi supera
+ * un valore letto come primo numero della sequenza
+ */
+
+namespace esercizio2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEsegui_Click(object sender, EventArgs e)
+        {
+            int input;
+            int numeroDaSup; // numero da superare
+            int accumulatore = 0;
+            
+            numeroDaSup = Convert.ToInt32(txtNDaSuperare.Text);
+
+            do
+            {
+                input = Convert.ToInt32(Interaction.InputBox("Inserisci numero intero positivo"));
+                accumulatore += input;
+                txtSommaNumeri.Text = accumulatore.ToString();
+
+            } while (accumulatore < numeroDaSup);
+        }
+    }
+}
